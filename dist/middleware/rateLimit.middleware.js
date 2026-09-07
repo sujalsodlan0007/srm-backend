73399399fd46c1ab0f6651239999066d061aacd1
sujalsodlan <sujalsodlan0007@gmail.com>
2026-09-07 10:42:38 +0530

    ss

--- dist/middleware/rateLimit.middleware.js
+++ dist/middleware/rateLimit.middleware.js
@@ -5,13 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.leadRateLimiter = void 0;
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const env_1 = require("../config/env");
+const isDev = env_1.env.NODE_ENV !== 'production';
 exports.leadRateLimiter = (0, express_rate_limit_1.default)({
-    windowMs: 60 * 60 * 1000,
-    max: 5,
+    windowMs: 15 * 60 * 1000,
+    max: isDev ? 100 : 30,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
         success: false,
-        message: 'Too many submissions from this IP, please try again later.'
+        message: 'Too many submissions from this IP, please try again in a few minutes.'
     }
 });
--- server/src/middleware/rateLimit.middleware.ts
+++ server/src/middleware/rateLimit.middleware.ts
@@ -1,12 +1,15 @@
 import rateLimit from 'express-rate-limit';
+import { env } from '../config/env';
+
+const isDev = env.NODE_ENV !== 'production';
 
 export const leadRateLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 5,
+  windowMs: 15 * 60 * 1000,
+  max: isDev ? 100 : 30,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
     success: false,
-    message: 'Too many submissions from this IP, please try again later.'
+    message: 'Too many submissions from this IP, please try again in a few minutes.'
   }
 });
